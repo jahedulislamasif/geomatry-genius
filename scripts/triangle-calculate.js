4bbca65deb661eda20073aa0e80926a7d3f5b38b
jahedulislamasif <jahedulislamasig.jia@gmail.com>
2024-02-12 22:32:52 +0600

    first commit

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,20 @@
+function calculateTriangleArea(){
+    // triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(base)
+    // triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText)
+
+    // calculate triangle area
+    const area = 0.5 * base * height;
+    console.log('Area of the triangle is :',area);
+
+    // display triangle area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+}
+
